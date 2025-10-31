48fde1aee73d7e343e085b4176176a30631097aa
My PC <My PC@DESKTOP-JU81ODE>
2025-10-31 09:51:28 +0900

    노트북 작업

--- src/Test/HelloGit.java
+++ src/Test/HelloGit.java
@@ -1,14 +1,11 @@
 package Test;
 
-<<<<<<< HEAD
 public interface HelloGit {
 
 	public static void main(String[] args) {
 		
 		System.out.println("1.첫번째 파일입니다.");
+		System.out.println("2.노트북 클론 작업 완료");
 	}
-=======
-public class HelloGit {
 
->>>>>>> refs/remotes/origin/main
 }
