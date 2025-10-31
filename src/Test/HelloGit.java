26d55a78e080234bdb4152433f6eacbc53ae73d9
uribosuk3 <uribosuk3@gmail.com>
2025-10-31 09:51:01 +0900

    메세지

--- src/Test/HelloGit.java
+++ src/Test/HelloGit.java
@@ -1,14 +1,10 @@
 package Test;
 
-<<<<<<< HEAD
 public interface HelloGit {
 
 	public static void main(String[] args) {
 		
 		System.out.println("1.첫번째 파일입니다.");
+		System.out.println("2.안녕하세요.");
 	}
-=======
-public class HelloGit {
-
->>>>>>> refs/remotes/origin/main
 }
